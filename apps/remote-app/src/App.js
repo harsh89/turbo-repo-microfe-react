f59a1941d0e537d548db32f13553628e1b76f95f
Harshvardhan <harshvardhan2001@gmail.com>
2023-04-13 12:33:59 -0400

    added external turbo package back

--- apps/remote-app/src/App.js
+++ apps/remote-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import { Link } from "ui";
+import { Link } from "ui";
 
 export const App = () => {
   return <div>Hello from the Remote App
-    {/* <Link className="App-link" href="https://reactjs.org">
+    <Link className="App-link" href="https://reactjs.org">
       React Docs
-    </Link> */}
+    </Link>
   </div>;
 };
 export default App;
